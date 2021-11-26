9d616d5f6e210c6f6d31b810f3bea21523db10c6
Quijotengineer <inigo.monreal@bath.edu>
2021-11-26 14:56:02 +0100

    fixed inputs validation

--- script.js
+++ script.js
@@ -7,19 +7,28 @@ function compute()
     var interest = principal * years * rate / 100;
     var year = new Date().getFullYear()+parseInt(years);
 
-    document.getElementById("result").innerHTML="\<br\>If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
+    validatePrincipal();
+
+    document.getElementById("result").innerHTML="\<br\>If you deposit " +
+    "<span class='highlight'>" + principal + "</span>" +
+    ",\<br\>at an interest rate of " +
+    "<span class='highlight'>"+ rate + "</span>" +
+    "%\<br\>You will receive an amount of " +
+    "<span class='highlight'>" + interest + "</span>" +
+    ",\<br\>in the year " + "<span class='highlight'>" + year + "</span>" + "\<br\>"
 }
         
 function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
+    document.getElementById("rate_val").innerText=rateval + "%";
 }
 
 function validatePrincipal()
 {
     var principal = document.getElementById("principal").value;
-    if (principal <= 0) {
+    // check if principal exists, is defined and a positive number
+    if (!(principal > 0) || !principal) {
         alert("Enter a positive number");
         fprincipal.focus();
         return false;
